aa39c12020b097224b174e48420c4b6c314d8ce2
aryanbroy <136257977+aryanbroy@users.noreply.github.com>
2024-02-09 15:59:40 +0530

    basic message board website

---
+++ routes/index.js
@@ -0,0 +1,35 @@
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date()
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date()
+  }
+];
+
+let express = require('express');
+let router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { 
+    title: 'Mini Messageboard',
+    messages: messages,
+  });
+});
+
+router.post("/new", (req, res, next) => {
+  messages.push({
+    text: req.body.message,
+    user: req.body.authorName,
+    added: new Date()
+  });
+  console.log(req.body)
+  res.redirect("/")
+})
+
+module.exports = router;
